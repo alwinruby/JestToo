1121d25b58d4235a56fa35c9d7f90a13fed5b559
asolanky <alwinsolanky@gmail.com>
2019-05-08 22:04:29 +0100

    updated test

--- id.spec.js
+++ id.spec.js
@@ -1,4 +1,13 @@
 // id.spec.js
-test('Jest is working', () => {
-   expect(1).toBe(1);
+// test('Jest is working', () => {
+//    expect(1).toBe(1);
+// });
+
+describe('First group of tests', () => {
+   test('Jest is working', () => {
+      expect(1).toBe(1);
+   });
+});
+describe('Another group of tests', () => {
+   // ...more tests here
 });
