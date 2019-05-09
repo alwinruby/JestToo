588c5b4c1c3d516e09d7c989d581a90c48885b18
asolanky <alwinsolanky@gmail.com>
2019-05-09 16:08:22 +0100

    rewritten test 1

--- id.spec.js
+++ id.spec.js
@@ -15,12 +15,22 @@ function getNewId() {
 
 describe('Requirements', () => {
 
+  // test('returns a random number', () => {
+  //   const mockMath = Object.create(global.Math);
+  //   mockMath.random = jest.fn(() => 0.75);
+  //   global.Math = mockMath;
+  //   const id = getNewId();
+  //   expect(id).toBe(0.75);
+  // });
+
   test('returns a random number', () => {
-    const mockMath = Object.create(global.Math);
-    mockMath.random = jest.fn(() => 0.75);
-    global.Math = mockMath;
-    const id = getNewId();
-    expect(id).toBe(0.75);
+   const originalMath = Object.create(global.Math);
+   const mockMath = Object.create(global.Math);
+   mockMath.random = () => 0.75;
+   global.Math = mockMath;
+   const id = getNewId();
+   expect(id).toBe(0.75);
+   global.Math = originalMath;
   });
 
   // test('returns an integer', () => {
